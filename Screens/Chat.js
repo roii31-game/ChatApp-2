c395b318d2a41963a087aebc77fa5780c790c22a
roii31-game <74428415+roii31-game@users.noreply.github.com>
2022-11-27 18:24:10 -0600

    Chat Finished

--- Screens/Chat.js
+++ Screens/Chat.js
@@ -52,7 +52,7 @@ export default function Chat() {
       setMessages(
         snapshot.docs.map((doc) => ({
           _id: doc.id,
-          createdAt: doc.data().createdAt,
+          createdAt: doc.data().createdAt.toDate(),
           text: doc.data().text,
           user: doc.data().user,
         }))
@@ -83,6 +83,7 @@ export default function Chat() {
         avatar:
           "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
       }}
+      messagesContainerStyle={{ backgroundColor: "#fff" }}
     />
   );
 }
